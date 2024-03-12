92581a5a8f8f7d9bcdb439c696706fb6d1c9fd67
mentorvikash <mentorvikash@gmail.com>
2024-03-13 00:40:24 +0530

    add css and styling

--- egg-store-frontend/src/App.jsx
+++ egg-store-frontend/src/App.jsx
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [eggData, setEggData] = useState({
+    count: 0,
+    price: 0,
+  });
+  const [isLoding, setIsLoding] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoding(true);
       try {
         const response = await fetch("http://localhost:3001/subscribe", {
           method: "POST",
@@ -19,7 +25,11 @@ function App() {
         const response1 = await fetch("http://localhost:3001/getDetail");
         const data1 = await response1.json();
         console.log({ data1 });
+        setEggData(data1.eggStock);
+        // setMessage(data1.eventMsg);
+        setIsLoding(false);
       } catch (error) {
+        setIsLoding(false);
         console.error("Error subscribing:", error);
       }
     };
@@ -27,10 +37,16 @@ function App() {
     fetchData();
   }, []);
 
+  if (isLoding) {
+    return <h1>Loading....</h1>;
+  }
+
   return (
-    <div>
-      <h1>Egg Store Webhook Example</h1>
-      <p>{message}</p>
+    <div className="store">
+      <h1 className="store__h1"> 🥚Eggs Store</h1>
+      <h2 className="store__count">count: {eggData?.count}</h2>
+      <h2 className="store__price">price: {eggData?.price}</h2>
+      <p className="store__message">{message}</p>
     </div>
   );
 }
